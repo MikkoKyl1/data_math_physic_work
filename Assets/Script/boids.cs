a4b534302ca144eee7d2d3d0b26060720a0a8721
Atte Hilander <attehilander@kamk.fi>
2020-12-01 13:10:23 +0200

    some changes

---
+++ Assets/Script/boids.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boids : MonoBehaviour
+{
+
+    public double x;
+    public double y;
+    public double Xvel;
+    public double Yvel;
+
+    public boids(double x, double y, double xVel, double Yvel)
+    {
+        (x, y, Xvel, Yvel) = (x, y, xVel, double yVel);
+    }
+}
