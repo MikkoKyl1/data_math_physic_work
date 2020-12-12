44bc4f623e34852f5dc34f7cf993ad5637f8334a
Atte Hilander <attehilander@kamk.fi>
2020-12-12 15:44:12 +0200

    Flock coding

--- Assets/Script/Flock.cs
+++ Assets/Script/Flock.cs
@@ -59,7 +59,7 @@ public class Flock : MonoBehaviour
         {
             List<Transform> context = GetNearbyObjects(agent);
 
-            agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.red, context.Count / 6f);
+            //agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.red, context.Count / 6f);
 
 
             Vector2 move = behavior.CalculateMove(agent, context, this);
